5726df5e6e9ee6d272aaf4869b2374d1697a9571
Jason Reeder <info@jasonreeder.com>
2017-03-02 17:12:12 -0800

    Add click listener to _all_ nav links

--- bundle.js
+++ bundle.js
@@ -71,8 +71,10 @@
 /***/ (function(module, exports) {
 
 document.addEventListener('DOMContentLoaded', () => {
-  const navLinks = document.querySelector('.sidebar-nav li');
+  const navLinks = document.querySelector('.sidebar-nav');
   navLinks.addEventListener("click", (e) => {
+    e.preventDefault();
+
     const element = e.target;
     const newLocation = element.innerText.toLowerCase();
     window.location.hash = newLocation;
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const navLinks = document.querySelector('.sidebar-nav li');
+  const navLinks = document.querySelector('.sidebar-nav');
   navLinks.addEventListener("click", (e) => {
+    e.preventDefault();
+
     const element = e.target;
     const newLocation = element.innerText.toLowerCase();
     window.location.hash = newLocation;
